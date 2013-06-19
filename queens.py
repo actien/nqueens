559933cf3091cb559217a7f4fa16dd2763c46ba7
Andy Tien <ac.tien@gmail.com>
2013-06-19 13:16:17 -0400

    Created Board and Position class. Basic testing of Board functionality is complete

--- queens.py
+++ queens.py
@@ -1,3 +1,38 @@
 # N queens
 # Andy Tien
-# 
+
+class Position:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+class Board:
+    def __init__(self, n):
+        self.valid_positions = []
+        # Make default positions
+        for i in range(0, n):
+            for j in range(0,n):
+                self.valid_positions.append(Position(i,j))
+
+        self.queens_left = n
+        self.size = n
+
+
+if __name__ == "__main__":
+    board = Board(1)
+    for pos in board.valid_positions:
+        print pos
+    print ""
+    board = Board(2)
+    for pos in board.valid_positions:
+        print pos
+    print ""
+    board = Board(3)
+    for pos in board.valid_positions:
+        print pos
+    print ""
+    board = Board(4)
+    for pos in board.valid_positions:
+        print pos
